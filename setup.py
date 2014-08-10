3f3cdc87244f820e0e376af9a52994e2e2ea2831
Sam Rad <sam.nikobonyadrad@rwth-aachen.de>
2014-08-10 03:37:35 +0200

    init

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='PREServ',
-      version='1.0',
-      description='OpenShift App',
+setup(name='COMSYS',
+      version='1.1',
+      description='Comsys CherryPy',
       author='Sam Rad',
       author_email='sam.nikobonyadrad@rwth-aachen.de',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+      install_requires=['CherryPy>=3.2.4'],
+      )
---
+++ wsgi/application.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import json
+import string
+import random
+
+sys.stdout = sys.stderr
+
+import atexit
+import threading
+import cherrypy
+# from utils.coords_classifier import CoordsClassifier
+
+# Path to static directory
+repo = os.environ['OPENSHIFT_REPO_DIR']
+STATIC_DIR = os.path.join(repo, u"wsgi", u"static")
+
+cherrypy.config.update({'environment': 'embedded'})
+
+if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
+    cherrypy.engine.start(blocking=False)
+    atexit.register(cherrypy.engine.stop)
+
+
+class Root(object):
+    _cp_config = {'tools.staticdir.on': True,
+                  'tools.staticdir.dir': STATIC_DIR,
+                  'tools.staticdir.index': 'bs-index.html',
+    }
+
+    @cherrypy.expose
+    def index(self):
+        return open(os.path.join(STATIC_DIR, u'bs-index.html'))
+
+    @cherrypy.expose
+    def generate(self, coords):
+        return coords
+        # return ''.join(random.sample(string.hexdigits, int(coords)))
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def monkey(self):
+        polys = open(os.path.join(STATIC_DIR, u'polygons.json'))
+        polys_json = json.load(polys)
+        return polys_json
+
+    @cherrypy.expose
+    def rhino(self):
+        return "Shit"
+        # classifier = CoordsClassifier()
+        # classifier.read_json()
+        # return classifier.classify()
+
+
+application = cherrypy.Application(Root(), script_name=None, config=None)
+
