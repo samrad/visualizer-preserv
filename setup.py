6cb90d566f5f4fcc2818884be8a3fcf53011e34b
Sam Rad <sam.nikobonyadrad@rwth-aachen.de>
2014-08-10 02:04:35 +0200

    PREServ requirements added

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='PREServ',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Sam Rad',
+      author_email='sam.nikobonyadrad@rwth-aachen.de',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
